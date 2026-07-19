7baf5e508b114e9e13594df69b807516bf94b1dc
razaeldotexe <razaelsaputra863@gmail.com>
2026-07-19 17:19:28 +0700

    fix: hardcode mantine colors to avoid client component error in server layout

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,5 +1,4 @@
 import { createTheme } from "@mantine/core";
-import { generateColors } from "@mantine/colors-generator";
 
 const theme = createTheme({
   breakpoints: {
@@ -10,10 +9,10 @@ const theme = createTheme({
     xl: "88em",
   },
   colors: {
-    brand: generateColors("#493ae1"),
-    visualizer: generateColors("#ae4bec"),
-    stt: generateColors("#34c5b2"),
-    metadata: generateColors("#fc9e5b"),
+    brand: ["#eeecff","#d9d5fe","#afa8f4","#8379ec","#5d50e4","#493ae1","#3929e0","#2b1dc7","#2419b3","#1a149e"],
+    visualizer: ["#fbeaff","#eed3fe","#d7a4f7","#c073f1","#ae4bec","#a12ee9","#9b1fe8","#8712cf","#780dba","#6803a3"],
+    stt: ["#e3fefa","#d4f6f2","#aeebe3","#85dfd3","#63d5c5","#4ccebd","#34c5b2","#2ab4a2","#19a090","#008b7c"],
+    metadata: ["#fff1e2","#ffe3cd","#fec59c","#fc9e5b","#fb8a3b","#fa791e","#fb700e","#df5f01","#c85300","#ae4500"],
   },
   primaryColor: "brand",
 });
